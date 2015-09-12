5009dfeae779c9fa96f7fb05e7ead568d8988b23
Leandro Lisboa Penz <lpenz@lpenz.org>
2015-09-12 13:18:00 -0300

    Tests refactored into different files; run only basic by default
    Travis still runs stress tests.

---
+++ tests/basic_tests.py
@@ -0,0 +1,61 @@
+'''Slickbird base tests'''
+
+import sys
+import os
+import json
+import logging
+
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+from tornado.testing import gen_test
+
+pjoin = os.path.join
+APP_ROOT = os.path.abspath(pjoin(os.path.dirname(__file__), '..'))
+sys.path.append(pjoin(APP_ROOT, '..'))
+
+from . import common
+
+
+def _log():
+    if not _log.logger:
+        _log.logger = logging.getLogger(__name__)
+    return _log.logger
+_log.logger = None
+
+
+class TestSlickbirdBase(common.TestSlickbirdBase):
+    @gen_test
+    def test_dummyscanner(self):
+        c = yield self.collectionadd(
+            'dummy',
+            pjoin(APP_ROOT, 'tests/dummytest.dat'))
+        self.assertEqual(len(c['games']), 1)
+        self.assertEqual(c['games'][0]['status'], 'missing')
+        self.assertFalse(
+            os.path.exists(pjoin(self.deploydir, 'emptyfile.txt')))
+        f = open(pjoin(self.scanningdir, 'emptyfile1.txt'), 'w')
+        f.close()
+        resp = yield self.http_client\
+            .fetch(self.get_url('/scanner'),
+                   method='POST',
+                   body=urlencode({'directory': self.scanningdir}),
+                   )
+        self.assertEqual(resp.code, 200)
+        fps = 'scanner'
+        while fps == 'scanner':
+            resp = yield self.http_client\
+                .fetch(self.get_url('/api/scanner.json'))
+            self.assertEqual(resp.code, 200)
+            fp = json.loads(resp.body.decode('utf-8'))
+            self.assertEqual(len(fp), 1)
+            self.assertEqual(
+                os.path.basename(fp[0]['filename']), 'emptyfile1.txt')
+            fps = fp[0]['status']
+        self.assertEqual(fps, 'moved')
+        self.assertTrue(
+            os.path.exists(pjoin(self.deploydir,
+                                 'dummy',
+                                 'emptyfile.txt')))
---
+++ tests/stress_tests.py
@@ -0,0 +1,32 @@
+'''Slickbird stress tests'''
+
+import sys
+import os
+import logging
+
+from tornado.testing import gen_test
+
+pjoin = os.path.join
+APP_ROOT = os.path.abspath(pjoin(os.path.dirname(__file__), '..'))
+sys.path.append(pjoin(APP_ROOT, '..'))
+
+from . import base
+
+
+def _log():
+    if not _log.logger:
+        _log.logger = logging.getLogger(__name__)
+    return _log.logger
+_log.logger = None
+
+
+class TestSlickbirdStress(base.TestSlickbirdBase):
+    @gen_test(timeout=300)
+    def test_bigdatadd(self):
+        filename = \
+            'Nintendo - Game Boy Advance Parent-Clone (20150801-084652).dat'
+        c = yield self.collectionadd(
+            'Game Boy',
+            pjoin(APP_ROOT, 'tests', filename))
+        _log().info('collection status {}, games {}'
+                    .format(c['collection']['status'], len(c['games'])))
