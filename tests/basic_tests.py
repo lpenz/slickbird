f99a2b1ac97ee2ed30fe4434faedb753fc8ec3c2
Leandro Lisboa Penz <lpenz@lpenz.org>
2015-09-12 22:33:26 -0300

    Test for static URLs

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -30,7 +30,10 @@ class TestSlickbirdBase(base.TestSlickbirdBase):
         self.assertEqual(c['games'][1]['status'], 'missing')
         self.assertFalse(
             os.path.exists(pjoin(self.deploydir, 'emptyfile.txt')))
-        f = open(pjoin(self.scanningdir, 'emptyfile1.txt'), 'w')
+        f = open(pjoin(self.scanningdir, 'emptyfile.txt'), 'w')
+        f.close()
+        f = open(pjoin(self.scanningdir, 'zfile.txt'), 'w')
+        f.write('z\n')
         f.close()
         resp = yield self.http_client\
             .fetch(self.get_url('/scanner/add'),
@@ -38,17 +41,18 @@ class TestSlickbirdBase(base.TestSlickbirdBase):
                    body=urlencode({'directory': self.scanningdir}),
                    )
         self.assertEqual(resp.code, 200)
-        fps = 'scanner'
-        while fps == 'scanner':
+        scanning = True
+        while scanning:
             resp = yield self.http_client\
                 .fetch(self.get_url('/api/scanner_lst.json'))
             self.assertEqual(resp.code, 200)
-            fp = json.loads(resp.body.decode('utf-8'))
-            self.assertEqual(len(fp), 1)
-            self.assertEqual(
-                os.path.basename(fp[0]['filename']), 'emptyfile1.txt')
-            fps = fp[0]['status']
-        self.assertEqual(fps, 'moved')
+            scannerlst = json.loads(resp.body.decode('utf-8'))
+            self.assertEqual(len(scannerlst), 2)
+            scannerdict = dict([(os.path.basename(s['filename']), s)
+                               for s in scannerlst])
+            self.assertEqual(set(scannerdict.keys()),
+                             set(['emptyfile.txt', 'zfile.txt']))
+            scanning = any([s['status'] == 'scanning' for s in scannerlst])
         self.assertTrue(
             os.path.exists(pjoin(self.deploydir,
                                  'dummy',
@@ -58,3 +62,19 @@ class TestSlickbirdBase(base.TestSlickbirdBase):
         self.assertEqual(c['games'][1]['status'], 'missing')
         c = yield self.collectionget('dummy', hidemissing=True)
         self.assertEqual(len(c['games']), 1)
+
+    @gen_test
+    def test_static_urls(self):
+        for f in [
+            '',
+            'collection_lst.jsx',
+            'game_lst.jsx',
+            'scanner_lst.jsx',
+            'collection/add',
+            'collection/list',
+            'scanner/add',
+            'scanner/list',
+        ]:
+            resp = yield self.http_client\
+                .fetch(self.get_url('/{}'.format(f)))
+            self.assertEqual(resp.code, 200)
